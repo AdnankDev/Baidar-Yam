d3face8a39f9e6b6aa21d6d58671db5409f2edbd
adnank <adnank.techs@gmail.com>
2026-04-13 13:56:54 +0500

    Add setup for register apoi

--- actions/register.ts
+++ actions/register.ts
@@ -1,12 +1,35 @@
 "use server";
 import * as z from 'zod';
 import { RegisterSchema } from '@/schemas';
+import bcrypt from "bcrypt";
+import { db } from '@/lib/db';
+import { getUserByEmail } from '@/data/user';
+
 
 export const register = async (values: z.infer<typeof RegisterSchema>)=>{
     const isValidate = RegisterSchema.safeParse(values);
     
-    if(!isValidate){
+    if(!isValidate.success){
         return {error: "Invalid Input"};
     };
-    return {success: "Register"};
+
+    const { email , password , name } = isValidate.data;
+    const hashPassword = await bcrypt.hash(password, 10)
+
+    const existingUser = await getUserByEmail(email);
+
+    if(existingUser) {
+        return {error: "Email already exist"};
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            passwrod: hashPassword,
+        }
+    });
+
+    // TODO: Send verifcation token email
+    return {success: "User Created!"};
 };
--- lib/db.ts
+++ lib/db.ts
@@ -2,9 +2,16 @@ import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+// 1. Ensure the adapter is initialized correctly
+const connectionString = process.env.DATABASE_URL;
+if (!connectionString) throw new Error("DATABASE_URL is missing");
 
-export { prisma };
+const adapter = new PrismaPg({ connectionString }); 
+
+// 2. Initialize Prisma Client with the adapter
+export const db = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+
+// 3. Save to global object in development to prevent multiple instances
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
